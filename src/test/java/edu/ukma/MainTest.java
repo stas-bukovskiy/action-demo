3e6cdeb3d23d2bf2d1b43e273696c864c20b6a32
stas-bukovskiy <stbukovskiy12@gmail.com>
2023-06-23 11:00:37 +0300

    add alwaysFailed test

---
+++ src/test/java/edu/ukma/MainTest.java
@@ -0,0 +1,13 @@
+package edu.ukma;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void alwaysFailed() {
+        assertTrue(false);
+    }
+}
